6022cc730e31ee81dc30bab6daed134b7437bd67
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2021-10-26 11:48:03 +0300

    Work.

--- Journal.py
+++ Journal.py
@@ -1,6 +1,9 @@
 import sys
 import traceback as tb
-from PyQt5.QtWidgets import QApplication
+
+from PyQt5 import QtCore
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QSplashScreen
 
 from classes.cl_const import Const
 from classes.cl_logwriter import LogWriter
@@ -20,6 +23,10 @@ if __name__ == '__main__':
     if Const.TEST_MODE:
         print('app = QApplication(sys.argv)')
     app = QApplication(sys.argv)
+    spl = QSplashScreen(QPixmap('Splash/Splash01-02.PNG'))
+    spl.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
+    spl.show()
+
     if Const.TEST_MODE:
         print('flog = LogWriter()')
     flog = LogWriter()
@@ -35,12 +42,15 @@ if __name__ == '__main__':
     if Const.TEST_MODE:
         print('login_user = LoginDialog(con)')
     login_user = LoginDialog(con)
+    spl.finish(login_user)
+    login_user.show()
     if Const.TEST_MODE:
         print('app.exec()')
     app.exec()
     if Const.TEST_MODE:
         print('if login_user.passwd_ok:')
     if login_user.passwd_ok:
+        spl.show()
         # print(login_user.loggedUser['id'])
         flog.to_log(f"""{login_user.loggedUser['id']}, {login_user.loggedUser['name'].strip()}, Успешный вход""")
         if Const.TEST_MODE:
@@ -48,6 +58,7 @@ if __name__ == '__main__':
         wnd = MWindow(con, login_user.loggedUser['id'])
         if Const.TEST_MODE:
             print('wnd.showMaximized()')
+        spl.finish(wnd)
         wnd.showMaximized()
         if Const.TEST_MODE:
             print('sys.exit(app.exec())')
